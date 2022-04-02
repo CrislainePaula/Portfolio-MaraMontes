ce381f86f50d98a22655ab8548680508f8de2b57
Crislaine Paula <96801822+CrislainePaula@users.noreply.github.com>
2022-04-02 18:21:02 -0300

    Atualização

--- interacao/script.js
+++ interacao/script.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrolly > 20){
@@ -12,4 +11,19 @@ $(document).ready(function(){
     
 });  
 
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
 
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 7000);
